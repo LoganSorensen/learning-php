6b77e051b6d9ce99f6f30cb9aa64c679f97a02c7
Logan Sorensen <loganmsorensen@gmail.com>
2020-08-07 20:34:56 -0700

    added website6

--- website3/index.php
+++ website3/index.php
@@ -55,7 +55,7 @@ if (filter_has_var(INPUT_POST, 'submit')) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel='stylesheet' href="bootstrap.min.css">
+    <link rel='stylesheet' href="../bootstrap.min.css">
     <title>Contact Us</title>
 </head>
 
