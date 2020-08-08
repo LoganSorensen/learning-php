1abd7b3d212a40bd428f830313357e394d300321
Logan Sorensen <loganmsorensen@gmail.com>
2020-08-07 22:38:55 -0700

    added website8

---
+++ website8/config/config.php
@@ -0,0 +1,6 @@
+<?php
+define('ROOT_URL', 'http://localhost/learning-php/website8/');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 's9jIYL3BSN56zZ0V');
+define('DB_NAME', 'phpblog');
